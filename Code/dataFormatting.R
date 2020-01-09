97874011cd3929b039b30dc1c42a0631f13aed77
Ariein2 <46645506+Ariein2@users.noreply.github.com>
2020-01-09 14:58:41 +0100

    PCA good2

--- Code/dataFormatting.R
+++ Code/dataFormatting.R
@@ -65,23 +65,20 @@ index_control <- sampleGroups == 'control'
 sampleGroups$id[index_control[,1]] <- 3
 
 ## PCA plots 
-long_microrna<-  data.frame(stack(mirna[,2:29], names(mirna[,2:29])))
-long_format_all<- merge (long_microrna, sampleGroups$id)
 
 mirna[is.na(mirna)] <- NA # change Nan for NA
-pcaRes <- pca(mirna[,2:29],nPcs = 10)  
+pcaRes <- pca(mirna[,2:29],nPcs = 10)  # perform PCA
 PCAtry<- data.frame(c(pcaRes@scores[,1]),
                     pcaRes@scores[,2],
                     pcaRes@scores[,3],
                     pcaRes@scores[,4],
                     pcaRes@scores[,5],
-                    treat=labels$V2)
+                    treat=labels$V2) 
 colnames(PCAtry) = c("PCA1", "PCA2", "PCA3", "PCA4", "PCA5","treat")
 
 ggplot(PCAtry, aes(x = PCA1, y = PCA2)) +
   geom_point(aes(colour = PCAtry$treat)) +
-  scale_colour_manual(labels = c("chol", "dil", "contr"),
-                      values = c("#04179b", "#da9e00", "#198c19"),
+  scale_colour_manual(values = c("#04179b", "#da9e00", "#198c19"),
                       aesthetics = "fill") +
   theme_light()
 
