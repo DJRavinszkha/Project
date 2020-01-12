9090b26936021251c0a2829baaa81c8f148b3a7d
Ariein2 <46645506+Ariein2@users.noreply.github.com>
2020-01-12 22:39:24 +0100

    PCA for batches and t test check
    PCA for batches.
    check if p  adjvalue and q values are the same.
    Store data for t test in data frames that are different depending on the treatment.

--- Code/PCA.R
+++ Code/PCA.R
@@ -90,3 +90,45 @@ fviz_dend(res.hcpc,
           labels_track_height = 0.8      # Augment the room for labels
 )
 
+#############  PCA FOR BATCHES  ######################
+
+mrna = read.delim('../Data/GeneExpressionNormalized.txt', check.names = FALSE) #Load expression data
+mrna = subset(mrna, select = -2) # Remove gene symbol column
+names_mrna = colnames(mrna[,2:length(mrna)]) #
+batch_number_mrna <- data.frame(Batch_num= substr(unlist(names_mrna), 8, 13)) #Only keep the batch number 
+name_batches<- unique(batch_number_mrna)
+
+mrna[is.na(mrna)] <- NA # change Nan for NA
+pcaRes2 <- pca(t(mrna[,2:29]),nPcs = 10)  # perform PCA
+PCA_28mrna<- data.frame(c(pcaRes2@scores[,1]),
+                        pcaRes2@scores[,2],
+                        pcaRes2@scores[,3],
+                        pcaRes2@scores[,4],
+                        pcaRes2@scores[,5],
+                        batch= batch_number_mrna) 
+colnames(PCA_28mrna) = c("PCA1", "PCA2", "PCA3","PCA4", "PCA5", "Batches")
+
+ggplot(PCA_28mrna, aes(x = PCA1, y = PCA2)) +
+  geom_point(aes(colour = PCA_28mrna$Batches)) +
+  scale_colour_manual(values = c("#04179b", "#da9e00", "#198c19","#66049b"),
+                      aesthetics = "fill") +
+  theme_light()
+
+mirna = read.delim('../Data/miRNAexpression.txt', check.names = FALSE)
+batch_number_mirna <- data.frame(Batch_num= substr(unlist(colnames(mirna[,2:length(mirna)])), 12, 23)) #Only keep the batch number 
+name_batches_mirna<- unique(batch_number_mirna)
+
+pcaRes <- pca(t(mirna[,2:29]),nPcs = 10)  # perform PCA
+PCA_28mirna<- data.frame(c(pcaRes@scores[,1]),
+                        pcaRes@scores[,2],
+                        pcaRes@scores[,3],
+                        pcaRes@scores[,4],
+                        pcaRes@scores[,5],
+                        batch= batch_number_mirna) 
+colnames(PCA_28mirna) = c("PCA1", "PCA2", "PCA3","PCA4", "PCA5", "Batches")
+
+ggplot(PCA_28mirna, aes(x = PCA1, y = PCA2)) +
+  geom_point(aes(colour = PCA_28mirna$Batches)) +
+  scale_colour_manual(values = c("#04179b", "#da9e00", "#198c19","#66049b"),
+                      aesthetics = "fill") +
+  theme_light()
